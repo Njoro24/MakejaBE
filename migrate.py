4f624e6b9bc89a4e74fd5d5f562ac26dc5dd9df3
+ <+>
2025-08-01 01:35:30 +0300

    fixed db

--- migrate.py
+++ migrate.py
@@ -1,9 +1,10 @@
-from app import create_app, db
+from app import create_app
 
 def deploy():
     app = create_app()
     with app.app_context():
         print("Creating all tables...")
+        from app.db import db
         db.create_all()
         print("Tables created!")
 
