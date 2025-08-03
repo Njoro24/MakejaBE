c1fd3c0965bb5b93c422dbdc9961a0c4b1e4ab68
Fancy Chepngetich Byegon <fancychepngetichbyegon@gmail.com>
2025-08-03 10:15:42 +0300

    fix importation errors

--- app/schemas/hostel_schema.py
+++ app/schemas/hostel_schema.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, validate, validates_schema, ValidationError
+from marshmallow import Schema, fields, validate, ValidationError
 
 
 class HostelCreateSchema(Schema):
